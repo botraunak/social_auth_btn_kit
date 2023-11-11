ff8483bec3caa30804db0181dab1ef303c3194a7
botraunak <sett.raunak@gmail.com>
2023-11-11 22:14:28 +0400

    chore: added basic button

--- lib/social_auth_btn_kit.dart
+++ lib/social_auth_btn_kit.dart
@@ -1,7 +1,15 @@
 library social_auth_btn_kit;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class SocialAuthBtn extends StatelessWidget {
+  const SocialAuthBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: const Text('Login with Google'),
+    );
+  }
 }
